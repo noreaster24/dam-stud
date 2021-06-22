e0f028f68339338f66a5bb05fc437f9496761e20
Joe Porrazzo <joepa2624@me.com>
2021-06-21 21:28:56 -0700

    refactoring

--- server.js
+++ server.js
@@ -10,22 +10,22 @@ const hbs = exphbs.create({ helpers });
 
 const session = require('express-session');
 
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
+// const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
-const sess = {
-    secret: 'Super secret secret',
-    cookie: {},
-    resave: false,
-    saveUninitialized: true,
-    store: new SequelizeStore({
-        db: sequelize
-    })
-};
+// const sess = {
+//     secret: 'Super secret secret',
+//     cookie: {},
+//     resave: false,
+//     saveUninitialized: true,
+//     store: new SequelizeStore({
+//         db: sequelize
+//     })
+// };
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(session(sess));
+// app.use(session(sess));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
