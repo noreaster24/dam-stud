7341d3b796a2cc71912c2c77754a66d86a0c72d4
Joe Porrazzo <joepa2624@me.com>
2021-06-23 18:44:48 -0700

    formatting updates

--- server.js
+++ server.js
@@ -10,17 +10,17 @@ const hbs = exphbs.create({ helpers });
 
 const session = require('express-session');
 
-// const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
-// const sess = {
-//     secret: 'Super secret secret',
-//     cookie: {},
-//     resave: false,
-//     saveUninitialized: true,
-//     store: new SequelizeStore({
-//         db: sequelize
-//     })
-// };
+const sess = {
+    secret: 'Super secret secret',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
 
 const app = express();
 const PORT = process.env.PORT || 3001;
